c47f3f1a4c9889695b9809fcc754c2db56505142
llg <465675262@qq.com>
2020-05-18 03:52:54 +0800

    xxxx

--- sample/sample-server/src/main/java/com/little/g/springcloud/sample/service/EchoServiceImpl.java
+++ sample/sample-server/src/main/java/com/little/g/springcloud/sample/service/EchoServiceImpl.java
@@ -1,10 +1,16 @@
 package com.little.g.springcloud.sample.service;
 
 import com.little.g.springcloud.sample.api.EchoService;
+import com.little.g.springcloud.sample.mapper.AiAssistantMapper;
 import org.apache.dubbo.config.annotation.Service;
+
+import javax.annotation.Resource;
 
 @Service(protocol = "dubbo")
 public class EchoServiceImpl implements EchoService {
+
+	@Resource
+	private AiAssistantMapper aiAssistantMapper;
 
 	@Override
 	public String echo(String message) {
