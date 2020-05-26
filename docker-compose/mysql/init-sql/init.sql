25f722095f5ca7bbff7d3f83e7a07d179191dff2
vps <vps@shuzijiayuan.com>
2020-05-26 10:31:12 +0800

    set openvpn

--- docker-compose/mysql/init-sql/init.sql
+++ docker-compose/mysql/init-sql/init.sql
@@ -2,3 +2,7 @@ source /opt/sql/nacos-mysql.sql;
 source /opt/sql/pay-mysql.sql;
 source /opt/sql/user-mysql.sql;
 source /opt/sql/admin-mysql.sql;
+
+source /opt/sql/mall_schema.sql;
+source /opt/sql/mall_table.sql;
+source /opt/sql/mall_data.sql;
