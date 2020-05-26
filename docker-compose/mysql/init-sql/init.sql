0e9d52b45b122ae636b36853ac8041486ecefa40
vps <vps@shuzijiayuan.com>
2020-05-26 12:04:39 +0800

    set test

--- docker-compose/mysql/init-sql/init.sql
+++ docker-compose/mysql/init-sql/init.sql
@@ -3,6 +3,14 @@ source /opt/sql/pay-mysql.sql;
 source /opt/sql/user-mysql.sql;
 source /opt/sql/admin-mysql.sql;
 
+-- mall
 source /opt/sql/mall_schema.sql;
 source /opt/sql/mall_table.sql;
 source /opt/sql/mall_data.sql;
+
+-- apollo
+source /opt/sql/apolloconfigdb.sql;
+source /opt/sql/apolloportaldb.sql;
+
+
+
