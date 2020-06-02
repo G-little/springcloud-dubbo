501f87cfaf9b63caa32b033a35ce6cd90d812b75
vps <vps@shuzijiayuan.com>
2020-06-02 14:22:18 +0800

    将用户ID 转换为Int

--- admin/admin-http/src/main/java/com/little/g/springcloud/admin/web/interceptor/LoginInterceptor.java
+++ admin/admin-http/src/main/java/com/little/g/springcloud/admin/web/interceptor/LoginInterceptor.java
@@ -1,6 +1,7 @@
 package com.little.g.springcloud.admin.web.interceptor;
 
 import com.little.g.springcloud.admin.api.ResourcesService;
+import com.little.g.springcloud.admin.web.annotation.RequiresPermissions;
 import com.little.g.springcloud.admin.web.utils.SessionUtils;
 import com.little.g.springcloud.admin.web.vo.LoginSession;
 import com.little.g.springcloud.common.error.CommonErrorCodes;
@@ -8,6 +9,7 @@ import com.little.g.springcloud.common.exception.ServiceDataException;
 import jdk.nashorn.internal.ir.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -19,35 +21,38 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class LoginInterceptor extends HandlerInterceptorAdapter {
 
-	private static Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
+    private static Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
 
-	@Reference
-	private ResourcesService resourcesService;
+    @Reference
+    private ResourcesService resourcesService;
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-			Object handler) throws Exception {
-		LoginSession session = SessionUtils.getLoginSession(request);
-		if (session == null) {
-			throw new ServiceDataException(CommonErrorCodes.NOT_LOGIN);
-		}
-		SessionUtils.set(session.getAdminUser());
-		return resourcesService.hasPrivilege(request.getRequestURI(),
-				session.getAdminUser());
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) throws Exception {
+        LoginSession session = SessionUtils.getLoginSession(request);
+        if (session == null) {
+            throw new ServiceDataException(CommonErrorCodes.NOT_LOGIN);
+        }
+        HandlerMethod method = (HandlerMethod) handler;
+        RequiresPermissions methodAnnotation = method.getMethodAnnotation(RequiresPermissions.class);
+        String[] value = methodAnnotation.value();
+        SessionUtils.set(session.getAdminUser());
+        return resourcesService.hasPrivilege(request.getRequestURI(),
+                session.getAdminUser());
 
-	}
+    }
 
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response,
-			Object handler, ModelAndView modelAndView) throws Exception {
-		super.postHandle(request, response, handler, modelAndView);
-	}
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+                           Object handler, ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+    }
 
-	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
-			Object handler, Exception ex) throws Exception {
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+                                Object handler, Exception ex) throws Exception {
 
-		super.afterCompletion(request, response, handler, ex);
-	}
+        super.afterCompletion(request, response, handler, ex);
+    }
 
 }
