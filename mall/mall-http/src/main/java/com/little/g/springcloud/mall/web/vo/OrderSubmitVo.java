8b90cd9de7755f4755933090a5f161a6cb576fd7
vps <vps@shuzijiayuan.com>
2020-06-12 20:49:59 +0800

    将用户ID 转换为Int

---
+++ mall/mall-http/src/main/java/com/little/g/springcloud/mall/web/vo/OrderSubmitVo.java
@@ -0,0 +1,15 @@
+package com.little.g.springcloud.mall.web.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@ApiModel("订单提交结果Vo")
+@Data
+public class OrderSubmitVo {
+    @ApiModelProperty("订单ID")
+    private Integer orderId;
+    @ApiModelProperty("团购ID")
+    private Integer grouponLinkId;
+
+}
